07968f24daea8e051e5b36391825b9f21185bb0d
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-02-21 18:22:33 +0100

    Add Bootstrap, Material, color.scss. Add topmenu with basic styling. Add sidemenu with basic styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import {MatButton, MatFabButton} from "@angular/material/button";
+import { SideMenuComponent } from './shared/side-menu/side-menu.component';
 
 function initializeKeycloak(keycloak: KeycloakService) {
   return () =>
@@ -21,22 +25,23 @@ function initializeKeycloak(keycloak: KeycloakService) {
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, ToolbarComponent, SideMenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    KeycloakAngularModule
+    KeycloakAngularModule,
+    MatFabButton,
+    MatButton,
   ],
   providers: [
     {
       provide: APP_INITIALIZER,
       useFactory: initializeKeycloak,
       multi: true,
-      deps: [KeycloakService]
-    }
+      deps: [KeycloakService],
+    },
+    provideAnimationsAsync(),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
