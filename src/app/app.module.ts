6e3847ac253497d1e04bb3fa2fdba03a0ab98a2f
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-02-21 13:23:28 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://auth.regiobiomatch.de',
+        realm: 'regiobiomatch',
+        clientId: 'regiobiomatch-client'
+      },
+      initOptions: {
+        onLoad: "login-required",
+        flow: "standard"
+      }
+    });
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +26,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
