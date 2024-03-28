cf8dbdf8ad2c739bf9e81bbbcc09b0bdccc7c97e
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-03-28 09:29:31 +0100

    remove usage of proxy instead use environment for now

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
   THE_MEAL_DB_KEY: '9973533',
+  MASTERDATA: 'http://localhost:8087/api/v1',
 };
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environmentProduction = {
+  THE_MEAL_DB_KEY: '9973533',
+  MASTERDATA: 'https://masterdata.regiobiomatch.de/api/v1',
+};
