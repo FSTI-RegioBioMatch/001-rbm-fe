295091c1f1e4e4e7397083ba77d72de77975e9b8
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-03-28 09:52:29 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   THE_MEAL_DB_KEY: '9973533',
-  MASTERDATA: 'http://localhost:8087/api/v1',
+  MASTERDATA: 'https://masterdata.regiobiomatch.de/api/v1',
 };
+
+// MASTERDATA: 'http://localhost:8087/api/v1',
+// MASTERDATA: 'https://masterdata.regiobiomatch.de/api/v1',
