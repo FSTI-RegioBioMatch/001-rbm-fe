b8d7966cf8291d729d7bf0aa8d6e75b728eead94
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-06-17 12:21:06 +0200

    added basic info back

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,9 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MapComponent } from './map/map.component';
 import { NearOffersCardComponent } from './components/near-offers-card/near-offers-card.component';
 import { OfferService } from '../shared/offer.service';
+import { RecepieDetailsDialogComponent } from './recepie-details-dialog/recepie-details-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { SupabaseService } from '../shared/services/supabase.service';
 
 @Component({
   selector: 'app-new-dashboard',
@@ -25,10 +28,12 @@ import { OfferService } from '../shared/offer.service';
 })
 export class DashboardComponent implements OnInit {
   @ViewChild('carousel') carousel!: ElementRef;
+  randomRecipes: any[] = [];
+  constructor(private dialog: MatDialog, public offerService: OfferService, private supabaseService: SupabaseService) {}
 
-  constructor(public offerService: OfferService) {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getRecepies();
+  }
 
   scrollLeft() {
     this.carousel.nativeElement.scrollBy({ left: -150, behavior: 'smooth' });
@@ -44,10 +49,37 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  gustoRecipes() {
-    this.gustarService.searchRecipes('Käse').subscribe((searchData) => {
-      console.log('Search Results:', searchData);
-      this.searchData = searchData;
-    });
+  async getRecepies() {
+    try {
+      const { data, error } = await this.supabaseService.supabaseClient
+        .from('recipes')
+        .select('*');
+  
+      if (error) {
+        throw error;
+      }
+  
+      if (data && data.length > 0) {
+        const randomRecipes = this.getRandom(data, 3);
+        console.log(randomRecipes);
+        this.randomRecipes = randomRecipes;
+      }
+    } catch (error) {
+      console.error('Error fetching recipies:', error);
+    }
+  }
+  
+  getRandom(arr: any[], n: number) {
+    const result = new Array(n);
+    let len = arr.length;
+    const taken = new Array(len);
+    if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+      const x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
   }
 }
--- src/app/dashboard/recepie-details-dialog/recepie-details-dialog.component.ts
+++ src/app/dashboard/recepie-details-dialog/recepie-details-dialog.component.ts
@@ -16,40 +16,8 @@ export class RecepieDetailsDialogComponent implements OnInit {
   loading = true;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private gustarService: GustarService
   ) { }
 
   ngOnInit(): void {
-    this.fetchDetails();
   }
-
-  fetchDetails(): void {
-    this.gustarService.crawlRecipe(this.data.meal.source).pipe(
-      switchMap(details => 
-        this.gustarService.dietClassifier(details.ingredients).pipe(
-          map(dietClassification => {
-            this.data.meal.details = details;
-            this.data.meal.dietClassification = dietClassification;
-            this.loading = false;
-            console.log('MealDetails:', this.data.meal);
-          }),
-          catchError(dietError => {
-            console.error(`Error classifying diet for ${this.data.meal.source}`, dietError);
-            this.data.meal.details = details;
-            this.data.meal.dietClassification = {};
-            this.loading = false;
-            return of({});
-          })
-        )
-      ),
-      catchError(crawlError => {
-        console.error(`Error fetching details for ${this.data.meal.source}`, crawlError);
-        this.data.meal.details = {};
-        this.data.meal.dietClassification = {};
-        this.loading = false;
-        return of({});
-      })
-    ).subscribe();
-  }
-
 }
