8d8ac99fdd9e8819b38916455349df79810f090f
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-07-30 11:22:43 +0200

    prepared my recipes

--- src/app/my-recipes/my-recipes.component.ts
+++ src/app/my-recipes/my-recipes.component.ts
@@ -1,8 +1,9 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { PublicRecipeService } from '../shared/services/public-recipe.service';
-import { StoreService } from '../shared/store/store.service';
 import { Router } from '@angular/router';
+import { RecipeService } from '../shared/services/recipe.service';
+import { StoreService } from '../shared/store/store.service';
 import { PrivateRecipeTableComponent } from './components/private-recipe-table/private-recipe-table.component';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-recipes',
@@ -12,15 +13,37 @@ import { PrivateRecipeTableComponent } from './components/private-recipe-table/p
   styleUrl: './my-recipes.component.scss',
 })
 export class MyRecipesComponent implements OnInit, AfterViewInit {
+  recipes: any[] = [];
+  totalElements: number = 0;
+
   constructor(
-    private publicRecipeService: PublicRecipeService,
     private store: StoreService,
     private router: Router,
+    private recipeService: RecipeService
   ) {}
 
   ngAfterViewInit(): void {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store.selectedCompanyContext$
+      .pipe(
+        filter(company => company !== null), // Ensure a non-null value is emitted
+        switchMap(company =>
+          this.recipeService.getRecipesByCompanyId(0, 10, 'recipeName,asc')
+        )
+      )
+      .subscribe(
+        page => {
+          this.recipes = page.content;
+          this.totalElements = page.totalElements;
+          console.log('Recipes:', this.recipes);
+          console.log('Total Elements:', this.totalElements);
+        },
+        error => {
+          console.error('Error fetching recipes:', error);
+        }
+      );
+  }
 
   onClickCreateMenuPlan() {
     console.log('Create Menu Plan');
