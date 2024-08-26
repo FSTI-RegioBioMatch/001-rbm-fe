0d0efb75ccbd0a51b480eb4d5c1c2dff3400c05a
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-08-26 16:29:44 +0200

    Add apiUrl

--- src/app/shared/services/new-menuplan.service.ts
+++ src/app/shared/services/new-menuplan.service.ts
@@ -6,12 +6,13 @@ import { StoreService } from '../store/store.service';
 import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewMenuplanService {
-  private apiUrl = `${environment.API_CORE}/new-menu-plans`;
-
-  constructor(private http: HttpClient, private storeService: StoreService) {}
+  constructor(
+    private http: HttpClient,
+    private storeService: StoreService,
+  ) {}
 
   /**
    * Saves a new menu plan with the currently selected company ID.
@@ -21,17 +22,20 @@ export class NewMenuplanService {
    */
   saveMenuPlan(menuPlan: any): Observable<any> {
     return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
+      switchMap((company) => {
         if (!company || !company.id) {
           return throwError('No company selected or company ID is missing');
         }
         menuPlan.companyId = company.id;
-        return this.http.post<any>(`${this.apiUrl}`, menuPlan);
+        return this.http.post<any>(
+          `${environment.API_CORE}/new-menu-plans`,
+          menuPlan,
+        );
       }),
-      catchError(error => {
+      catchError((error) => {
         console.error('Error saving menu plan:', error);
         return throwError(error);
-      })
+      }),
     );
   }
 
@@ -43,17 +47,20 @@ export class NewMenuplanService {
    */
   deleteMenuPlan(menuPlanId: string): Observable<void> {
     return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
+      switchMap((company) => {
         if (!company || !company.id) {
           return throwError('No company selected or company ID is missing');
         }
         let params = new HttpParams().set('companyId', company.id);
-        return this.http.delete<void>(`${this.apiUrl}/${menuPlanId}`, { params });
+        return this.http.delete<void>(
+          `${environment.API_CORE}/new-menu-plans/${menuPlanId}`,
+          { params },
+        );
       }),
-      catchError(error => {
+      catchError((error) => {
         console.error('Error deleting menu plan:', error);
         return throwError(error);
-      })
+      }),
     );
   }
 
@@ -64,19 +71,25 @@ export class NewMenuplanService {
    * @param eventId - The ID of the event to delete.
    * @returns An Observable of the response.
    */
-  deleteEventFromMenuPlan(menuPlanId: string, eventId: string): Observable<any> {
+  deleteEventFromMenuPlan(
+    menuPlanId: string,
+    eventId: string,
+  ): Observable<any> {
     return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
+      switchMap((company) => {
         if (!company || !company.id) {
           return throwError('No company selected or company ID is missing');
         }
         let params = new HttpParams().set('companyId', company.id);
-        return this.http.delete<any>(`${this.apiUrl}/${menuPlanId}/events/${eventId}`, { params });
+        return this.http.delete<any>(
+          `${environment.API_CORE}/new-menu-plans/${menuPlanId}/events/${eventId}`,
+          { params },
+        );
       }),
-      catchError(error => {
+      catchError((error) => {
         console.error('Error deleting event from menu plan:', error);
         return throwError(error);
-      })
+      }),
     );
   }
 
@@ -88,17 +101,20 @@ export class NewMenuplanService {
    */
   getMenuPlanById(menuPlanId: string): Observable<any> {
     return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
+      switchMap((company) => {
         if (!company || !company.id) {
           return throwError('No company selected or company ID is missing');
         }
         let params = new HttpParams().set('companyId', company.id);
-        return this.http.get<any>(`${this.apiUrl}/${menuPlanId}`, { params });
+        return this.http.get<any>(
+          `${environment.API_CORE}/new-menu-plans/${menuPlanId}`,
+          { params },
+        );
       }),
-      catchError(error => {
+      catchError((error) => {
         console.error('Error fetching menu plan by ID:', error);
         return throwError(error);
-      })
+      }),
     );
   }
 
@@ -109,41 +125,50 @@ export class NewMenuplanService {
    */
   getAllMenuPlans(): Observable<any[]> {
     return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
+      switchMap((company) => {
         if (!company || !company.id) {
           return throwError('No company selected or company ID is missing');
         }
         let params = new HttpParams().set('companyId', company.id);
-        return this.http.get<any[]>(`${this.apiUrl}`, { params });
+        return this.http.get<any[]>(`${environment.API_CORE}/new-menu-plans`, {
+          params,
+        });
       }),
-      catchError(error => {
+      catchError((error) => {
         console.error('Error fetching all menu plans:', error);
         return throwError(error);
-      })
+      }),
     );
   }
   /**
- * Updates an event in a menu plan by IDs with the currently selected company ID.
- *
- * @param menuPlanId - The ID of the menu plan.
- * @param eventId - The ID of the event to update.
- * @param eventData - The updated event data.
- * @returns An Observable of the response.
- */
-updateEventInMenuPlan(menuPlanId: string, eventId: string, eventData: any): Observable<any> {
-  return this.storeService.selectedCompanyContext$.pipe(
-      switchMap(company => {
-          if (!company || !company.id) {
-              return throwError('No company selected or company ID is missing');
-          }
-          let params = new HttpParams().set('companyId', company.id);
-          return this.http.put<any>(`${this.apiUrl}/${menuPlanId}/events/${eventId}`, eventData, { params });
+   * Updates an event in a menu plan by IDs with the currently selected company ID.
+   *
+   * @param menuPlanId - The ID of the menu plan.
+   * @param eventId - The ID of the event to update.
+   * @param eventData - The updated event data.
+   * @returns An Observable of the response.
+   */
+  updateEventInMenuPlan(
+    menuPlanId: string,
+    eventId: string,
+    eventData: any,
+  ): Observable<any> {
+    return this.storeService.selectedCompanyContext$.pipe(
+      switchMap((company) => {
+        if (!company || !company.id) {
+          return throwError('No company selected or company ID is missing');
+        }
+        let params = new HttpParams().set('companyId', company.id);
+        return this.http.put<any>(
+          `${environment.API_CORE}/new-menu-plans/${menuPlanId}/events/${eventId}`,
+          eventData,
+          { params },
+        );
       }),
-      catchError(error => {
-          console.error('Error updating event in menu plan:', error);
-          return throwError(error);
-      })
-  );
-}
-
+      catchError((error) => {
+        console.error('Error updating event in menu plan:', error);
+        return throwError(error);
+      }),
+    );
+  }
 }
