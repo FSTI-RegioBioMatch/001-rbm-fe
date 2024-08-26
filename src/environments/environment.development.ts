c05bc3b37400f5851619320efd909b8e5a33ae2c
Daniel Burkhardt <danibu@gmx.net>
2024-08-26 16:18:15 +0200

    backend deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
   THE_MEAL_DB_KEY: '9973533',
   MASTERDATA: 'http://localhost:8080/api/v1',
   NEARBUY_API: 'https://api.staging.nearbuy-food.de/v1',
-  API_CORE: 'http://localhost:8082/api/v1',
+  //API_CORE: 'http://localhost:8082/api/v1',
+  API_CORE: 'https://core.regiobiomatch.de/api/v1',
 };
