ef6791446c05bd98580ef11c6c837731abbc66d9
Daniel Burkhardt <danibu@gmx.net>
2024-09-02 18:00:25 +0200

    update dashboard III

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,59 +1,92 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
 import { ChangeCompanyDialogComponent } from '../components/change-company-dialog/change-company-dialog.component';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
-import { Button } from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { DialogService } from 'primeng/dynamicdialog';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss',
+  styleUrls: ['./toolbar.component.scss'],
   standalone: true,
-  imports: [RouterLink, DialogModule, InputTextModule, Button, MenuModule],
+  imports: [
+    RouterLink,
+    DialogModule,
+    InputTextModule,
+    ButtonModule,
+    MenuModule,
+  ],
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
   visible: boolean = false;
-
-  menuItems: MenuItem[] = [
-    {
-      label: 'Options',
-      items: [
-        {
-          label: 'Übersicht',
-          routerLink: '/dashboard',
-        },
-        {
-          label: 'Rezepte',
-          routerLink: '/my-recipes',
-        },
-        {
-          label: 'Menüplanung',
-          routerLink: '/menu-planning',
-        },
-        {
-          label: '{Martkplatz}',
-          routerLink: '/recipes',
-        },
-        {
-          label: 'Bereich xy',
-          routerLink: '/recipes',
-        },
-      ],
-    },
-  ];
+  menuItems: MenuItem[] = [];
+  private routerSubscription: Subscription | undefined;
 
   constructor(
-    private route: Router,
+    private router: Router,
     private dialogService: DialogService,
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeMenu();
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateActiveMenuItem();
+      });
+  }
 
-  onClickOpenChangeCompanyDialog() {
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  initializeMenu(): void {
+    this.menuItems = [
+      {
+        label: 'Übersicht',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/dashboard',
+      },
+      {
+        label: 'Rezepte',
+        icon: 'pi pi-fw pi-book',
+        routerLink: '/my-recipes',
+      },
+      {
+        label: 'Menüplanung',
+        icon: 'pi pi-fw pi-calendar',
+        routerLink: '/menu-planning',
+      },
+      {
+        label: 'Marktplatz',
+        icon: 'pi pi-fw pi-shopping-cart',
+        routerLink: '/recipes',
+      },
+      {
+        label: 'Bereich xy',
+        icon: 'pi pi-fw pi-cog',
+        routerLink: '/recipes',
+      },
+    ];
+  }
+
+  updateActiveMenuItem(): void {
+    const currentUrl = this.router.url;
+    this.menuItems.forEach((item) => {
+      item.styleClass =
+        item.routerLink === currentUrl ? 'active-menu-item' : '';
+    });
+  }
+
+  onClickOpenChangeCompanyDialog(): void {
     const dialogRef = this.dialogService.open(ChangeCompanyDialogComponent, {
       header: 'Firma wechseln',
       width: '40%',
