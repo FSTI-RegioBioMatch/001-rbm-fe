4a4d3f411f62b9f4eda60a3027ef92b0edbd4490
Michael Krause <78827985+mkccl@users.noreply.github.com>
2024-09-11 11:01:14 +0200

    update dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { MyProductsComponent } from './products/my-products/my-products.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuPlanningComponent } from './menu-planning/menu-planning.component';
-import { NewRecepieDialogComponent } from './new-recepie-dialog/new-recepie-dialog.component';
 import { MyRecipesComponent } from './my-recipes/my-recipes.component';
-import { ShoppingListComponent } from './menu-planning/shopping-list/shopping-list.component';
 import { ShoppingListDetailedComponent } from './menu-planning/shopping-list/shopping-list-detailed/shopping-list-detailed.component';
-import { OfferScanComponent } from './menu-planning/shopping-list/offer-scan/offer-scan.component';
 import { MenuPlansComponent } from './menu-planning/menu-plans/menu-plans.component';
 import { RecipieDetailsComponent } from './recipie-details/recipie-details.component';
 import { MyMenusComponent } from './menu-planning/my-menus/my-menus.component';
@@ -17,11 +14,9 @@ import { NewOfferScanComponent } from './new-offer-scan/new-offer-scan.component
 import { RegisterComponent } from './register/register.component';
 import { MenuPlanDetailsComponent } from './menu-plan-details/menu-plan-details.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
-import { SeasonalCalendarComponent } from './dashboard2/components/seasonal-calendar/seasonal-calendar.component';
 
 const routes: Routes = [
   { path: '', component: Dashboard2Component },
-  { path: 'd/s', component: SeasonalCalendarComponent },
   {
     path: 'my-recipes',
     component: MyRecipesComponent,
--- src/app/dashboard2/components/seasonal-calendar/seasonal-calendar.component.ts
+++ src/app/dashboard2/components/seasonal-calendar/seasonal-calendar.component.ts
@@ -3,6 +3,7 @@ import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 import { SelectItem } from 'primeng/api';
+import { JsonPipe } from '@angular/common';
 
 interface Produce {
   name: string;
@@ -13,7 +14,7 @@ interface Produce {
 @Component({
   selector: 'app-seasonal-calendar',
   standalone: true,
-  imports: [DropdownModule, TableModule, FormsModule],
+  imports: [DropdownModule, TableModule, FormsModule, JsonPipe],
   templateUrl: './seasonal-calendar.component.html',
   styleUrl: './seasonal-calendar.component.scss',
 })
@@ -21,8 +22,7 @@ export class SeasonalCalendarComponent implements OnInit {
   months!: SelectItem[];
   selectedMonth: number = new Date().getMonth();
 
-  seasonalFruits: Produce[] = [];
-  seasonalVegetables: Produce[] = [];
+  outputData: Produce[] = [];
 
   produceData: Produce[] = [
     { name: 'Apfel', type: 'fruit', seasons: [1, 2, 3, 4, 9, 10, 11, 12] },
@@ -35,7 +35,34 @@ export class SeasonalCalendarComponent implements OnInit {
     },
     { name: 'Tomate', type: 'vegetable', seasons: [7, 8, 9, 10] },
     { name: 'Karotte', type: 'vegetable', seasons: [6, 7, 8, 9, 10, 11] },
-    // Fügen Sie hier weitere Obst- und Gemüsesorten hinzu
+    { name: 'Kirsche', type: 'fruit', seasons: [6, 7, 8] },
+    { name: 'Pflaume', type: 'fruit', seasons: [8, 9, 10] },
+    { name: 'Himbeere', type: 'fruit', seasons: [6, 7, 8, 9] },
+    { name: 'Blaubeere', type: 'fruit', seasons: [7, 8, 9] },
+    { name: 'Spargel', type: 'vegetable', seasons: [4, 5, 6] },
+    { name: 'Kürbis', type: 'vegetable', seasons: [9, 10, 11] },
+    { name: 'Kohlrabi', type: 'vegetable', seasons: [5, 6, 7, 8, 9, 10] },
+    { name: 'Rosenkohl', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2] },
+    { name: 'Grünkohl', type: 'vegetable', seasons: [11, 12, 1, 2] },
+    { name: 'Rhabarber', type: 'vegetable', seasons: [4, 5, 6] },
+    { name: 'Johannisbeere', type: 'fruit', seasons: [6, 7, 8] },
+    { name: 'Stachelbeere', type: 'fruit', seasons: [6, 7, 8] },
+    { name: 'Zwetschge', type: 'fruit', seasons: [8, 9, 10] },
+    { name: 'Mirabelle', type: 'fruit', seasons: [8, 9] },
+    { name: 'Quitte', type: 'fruit', seasons: [9, 10] },
+    { name: 'Brombeere', type: 'fruit', seasons: [8, 9, 10] },
+    { name: 'Heidelbeere', type: 'fruit', seasons: [7, 8, 9] },
+    { name: 'Holunderbeere', type: 'fruit', seasons: [8, 9, 10] },
+    { name: 'Zucchini', type: 'vegetable', seasons: [6, 7, 8, 9] },
+    { name: 'Aubergine', type: 'vegetable', seasons: [7, 8, 9, 10] },
+    { name: 'Paprika', type: 'vegetable', seasons: [7, 8, 9, 10] },
+    { name: 'Brokkoli', type: 'vegetable', seasons: [6, 7, 8, 9, 10] },
+    { name: 'Blumenkohl', type: 'vegetable', seasons: [6, 7, 8, 9] },
+    { name: 'Spinat', type: 'vegetable', seasons: [4, 5, 9, 10] },
+    { name: 'Feldsalat', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2, 3] },
+    { name: 'Rote Bete', type: 'vegetable', seasons: [7, 8, 9, 10, 11] },
+    { name: 'Pastinake', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2, 3] },
+    { name: 'Fenchel', type: 'vegetable', seasons: [6, 7, 8, 9, 10, 11] },
   ];
 
   ngOnInit() {
@@ -58,15 +85,8 @@ export class SeasonalCalendarComponent implements OnInit {
   }
 
   updateProduceList() {
-    this.seasonalFruits = this.produceData.filter(
-      (item) =>
-        item.type === 'fruit' && item.seasons.includes(this.selectedMonth + 1),
-    );
-
-    this.seasonalVegetables = this.produceData.filter(
-      (item) =>
-        item.type === 'vegetable' &&
-        item.seasons.includes(this.selectedMonth + 1),
+    this.outputData = this.produceData.filter((item) =>
+      item.seasons.includes(this.selectedMonth + 1),
     );
   }
 }
