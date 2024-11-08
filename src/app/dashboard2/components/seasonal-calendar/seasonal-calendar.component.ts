8b5bd1d44dc2d8a97ad75e8da77ef672f54d913c
Kulichkova <elena.kulichkova@ferdinand-steinbeis-institut.de>
2024-11-08 16:13:49 +0100

    get history data in SeasonalCalendarComponent; upload image; filter based on a season

--- src/app/dashboard2/components/seasonal-calendar/seasonal-calendar.component.ts
+++ src/app/dashboard2/components/seasonal-calendar/seasonal-calendar.component.ts
@@ -7,12 +7,21 @@ import { CommonModule, JsonPipe } from '@angular/common';
 import { PixabayService } from '../../../shared/services/pixabay.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HistoryOfferService} from '../../../shared/services/history-offer.service';
+import { HistoricProductType } from '../../../shared/types/historicproduct.type';
+import { StoreService } from '../../../shared/store/store.service'; 
+import { AddressType } from '../../../shared/types/address.type';
+import { Subscription } from 'rxjs';
 
 interface Produce {
   name: string;
-  type: 'fruit' | 'vegetable';
-  seasons: number[];
+  // type: 'fruit' | 'vegetable'; // no identifier
+  seasons?: number[];
   imageUrl?: string;
+  // links?
+  dateStart: string;
+  dateEnd: string;
+  isPermanent: boolean;
 }
 
 @Component({
@@ -22,56 +31,30 @@ interface Produce {
   templateUrl: './seasonal-calendar.component.html',
   styleUrls: ['./seasonal-calendar.component.scss'],
 })
+
 export class SeasonalCalendarComponent implements OnInit {
   months!: SelectItem[];
   selectedMonth: number = new Date().getMonth();
+  seasons!: SelectItem[];
+  loaded = false; // what for?
+  products: HistoricProductType[] = [];
+  filteredProducts: Produce[] = [];
+  displayDialog: boolean = false;
+  selectedProduct: HistoricProductType | null = null;
 
-  outputData: Produce[] = [];
+  outputData: Produce[] = []
+  produceData: Produce[] = []
 
-  produceData: Produce[] = [
-    { name: 'Apfel', type: 'fruit', seasons: [1, 2, 3, 4, 9, 10, 11, 12] },
-    { name: 'Erdbeere', type: 'fruit', seasons: [5, 6, 7, 8] },
-    { name: 'Birne', type: 'fruit', seasons: [8, 9, 10, 11] },
-    {
-      name: 'Kartoffel',
-      type: 'vegetable',
-      seasons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    },
-    { name: 'Tomate', type: 'vegetable', seasons: [7, 8, 9, 10] },
-    { name: 'Karotte', type: 'vegetable', seasons: [6, 7, 8, 9, 10, 11] },
-    { name: 'Kirsche', type: 'fruit', seasons: [6, 7, 8] },
-    { name: 'Pflaume', type: 'fruit', seasons: [8, 9, 10] },
-    { name: 'Himbeere', type: 'fruit', seasons: [6, 7, 8, 9] },
-    { name: 'Blaubeere', type: 'fruit', seasons: [7, 8, 9] },
-    { name: 'Spargel', type: 'vegetable', seasons: [4, 5, 6] },
-    { name: 'Kürbis', type: 'vegetable', seasons: [9, 10, 11] },
-    { name: 'Kohlrabi', type: 'vegetable', seasons: [5, 6, 7, 8, 9, 10] },
-    { name: 'Rosenkohl', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2] },
-    { name: 'Grünkohl', type: 'vegetable', seasons: [11, 12, 1, 2] },
-    { name: 'Rhabarber', type: 'vegetable', seasons: [4, 5, 6] },
-    { name: 'Johannisbeere', type: 'fruit', seasons: [6, 7, 8] },
-    { name: 'Stachelbeere', type: 'fruit', seasons: [6, 7, 8] },
-    { name: 'Zwetschge', type: 'fruit', seasons: [8, 9, 10] },
-    { name: 'Mirabelle', type: 'fruit', seasons: [8, 9] },
-    { name: 'Quitte', type: 'fruit', seasons: [9, 10] },
-    { name: 'Brombeere', type: 'fruit', seasons: [8, 9, 10] },
-    { name: 'Heidelbeere', type: 'fruit', seasons: [7, 8, 9] },
-    { name: 'Holunderbeere', type: 'fruit', seasons: [8, 9, 10] },
-    { name: 'Zucchini', type: 'vegetable', seasons: [6, 7, 8, 9] },
-    { name: 'Aubergine', type: 'vegetable', seasons: [7, 8, 9, 10] },
-    { name: 'Paprika', type: 'vegetable', seasons: [7, 8, 9, 10] },
-    { name: 'Brokkoli', type: 'vegetable', seasons: [6, 7, 8, 9, 10] },
-    { name: 'Blumenkohl', type: 'vegetable', seasons: [6, 7, 8, 9] },
-    { name: 'Spinat', type: 'vegetable', seasons: [4, 5, 9, 10] },
-    { name: 'Feldsalat', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2, 3] },
-    { name: 'Rote Bete', type: 'vegetable', seasons: [7, 8, 9, 10, 11] },
-    { name: 'Pastinake', type: 'vegetable', seasons: [9, 10, 11, 12, 1, 2, 3] },
-    { name: 'Fenchel', type: 'vegetable', seasons: [6, 7, 8, 9, 10, 11] },
-  ];
+  constructor(
+    private pixabayService: PixabayService,
+    private router: Router,
+    private http: HttpClient,
+    private historyOfferService: HistoryOfferService, 
+    // private cdr: ChangeDetectorRef,
+    private store: StoreService // Inject StoreService
+  ) {}
 
-  constructor(private pixabayService: PixabayService, private router: Router,private http: HttpClient) {}
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.months = [
       { label: 'Januar', value: 0 },
       { label: 'Februar', value: 1 },
@@ -87,31 +70,153 @@ export class SeasonalCalendarComponent implements OnInit {
       { label: 'Dezember', value: 11 },
     ];
 
+    this.seasons = [
+      { label: 'spring', value: [2, 3, 4] },
+      { label: 'summer', value: [5, 6, 7] },
+      { label: 'autumn', value: [8, 9, 10] },
+      { label: 'winter', value: [11, 0, 1] },
+    ];
+
+    this.getProductHistory();
     this.updateProduceList();
+    this.updateOutputList();
+  }
+  
+  ngOnDestroy(): void {
+    // unsubscibe() needed
+  }
+      
+  getProductHistory() {
+    this.store.selectedCompanyContext$.subscribe(company => {
+      if (company && company.addresses && company.addresses.length > 0) {
+        const addressUrl = company.addresses[0].self; // Use the first address
+        this.historyOfferService.getAddress(addressUrl).subscribe((address: AddressType) => {
+          const searchRadiusInKM = 50; // Adjust the search radius as needed
+          this.historyOfferService.setProductsBySearchCriteria(searchRadiusInKM, address);
+        });
+      }
+    });
+
+    // Subscribe to the loaded observable to update the loading state
+    this.historyOfferService.loaded$.subscribe(loaded => {
+      this.loaded = loaded;
+    });
   }
 
   updateProduceList() {
-    this.outputData = this.produceData.filter((item) =>
-      item.seasons.includes(this.selectedMonth + 1),
-    );
-    this.loadImagesForProduce(); 
+    // Subscribe to the products observable to update the products list
+    this.historyOfferService.products$.subscribe((products) => {
+      this.products = products;
+      // console.log('products: ', this.products);
+      this.products.forEach((product) => {
+        const productLabel = product?.ontoFoodType?.label;
+        if (productLabel) {
+          const capitalized = this.capitalizeFirstLetter(productLabel);
+          const exists = this.produceData.some(item => item.name === capitalized);
+          if (!exists)
+            this.produceData.push({name: capitalized,
+                                  dateStart: product?.product?.dateStart,
+                                  dateEnd: product?.product?.dateEnd,
+                                  isPermanent: product?.product?.isPermanent});
+        }
+      })
+    });
+  
+    // console.log('produceData: ', this.produceData);
   }
+
+  updateOutputList() {
+    const currentSeason = this.getSeason(this.selectedMonth);
+    if (currentSeason)
+      this.filterProductsBySeason(currentSeason);
+    this.loadImagesForProduce();
+    // console.log('outputData: ', this.outputData);
+  }
+
+  filterProductsBySeason(season: string): void {
+    const now = new Date();
+    this.outputData = this.produceData.filter(product => {
+      if (product.isPermanent) {
+        return true; // Always available
+      }
+  
+      const dateStart = this.parseDate(product.dateStart);
+      const dateEnd = this.parseDate(product.dateEnd);
+  
+      if (!dateStart || !dateEnd) {
+        return false; // Skip products with invalid or null dates
+      }
+  
+      switch (season) {
+        case 'spring':
+          return this.isDateInRange(dateStart, dateEnd, new Date(now.getFullYear(), 2, 21), new Date(now.getFullYear(), 5, 20));
+        case 'summer':
+          return this.isDateInRange(dateStart, dateEnd, new Date(now.getFullYear(), 5, 21), new Date(now.getFullYear(), 8, 22));
+        case 'autumn':
+          return this.isDateInRange(dateStart, dateEnd, new Date(now.getFullYear(), 8, 23), new Date(now.getFullYear(), 11, 20));
+        case 'winter':
+          return this.isDateInRange(dateStart, dateEnd, new Date(now.getFullYear(), 11, 21), new Date(now.getFullYear() + 1, 2, 20));
+        default:
+          return false;
+      }
+    });
+  }
+  
+  parseDate(dateString: string): Date | null {
+    if (!dateString) return null;
+    const date = new Date(dateString);
+    return isNaN(date.getTime()) ? null : date;
+  }
+
+  isDateInRange(start: Date, end: Date, rangeStart: Date, rangeEnd: Date): boolean {
+    // Check if the date range overlaps with the season range
+    return (start <= rangeEnd && end >= rangeStart);
+  }
+
+  trackByFn(index: number, item: any): any {
+    return item.company.id; // or item.someUniqueIdentifier
+  }
+
+  showDetails(product: HistoricProductType): void {
+    this.selectedProduct = product;
+    this.displayDialog = true;
+    // this.cdr.detectChanges(); // Manually trigger change detection
+  }
+
+  logToConsole(message: string): void {
+    console.log(message);
+  }
+
+  clearFilteredProducts(): void {
+    this.filteredProducts = [];
+  }
+  
   loadImagesForProduce() {
     this.outputData.forEach((produce) => {
-        this.pixabayService.searchImage(produce.name).subscribe({
-            next: (response: any) => {
-                if (response.hits && response.hits.length > 0) {
-                    produce.imageUrl = response.hits[0].webformatURL;  // Set the first image URL
-                } else {
-                    produce.imageUrl = '';  // No image found
-                    console.warn(`Kein Bild gefunden für ${produce.name}`);
-                }
-            },
-            error: (error) => {
-                console.error(`Fehler beim Abrufen des Bildes für ${produce.name}:`, error);
-                produce.imageUrl = '';  // In case of an error, fallback to no image
-            }
-        });
+      this.pixabayService.searchImage(produce.name).subscribe({
+        next: (response: any) => {
+          if (response.hits && response.hits.length > 0) {
+            produce.imageUrl = response.hits[0].webformatURL;  // Set the first image URL
+          } else {
+            produce.imageUrl = '';  // No image found
+            console.warn(`Kein Bild gefunden für ${produce.name}`);
+          }
+          },
+          error: (error) => {
+            console.error(`Fehler beim Abrufen des Bildes für ${produce.name}:`, error);
+            produce.imageUrl = '';  // In case of an error, fallback to no image
+          }
+      });
     });
-}
+  }
+  
+  capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
+  // Function to get the season based on the selected month
+  getSeason(selectedMonth: number) {
+    const season = this.seasons.find(season => season.value.includes(selectedMonth));
+    return season?.label;
+  }
 }
--- src/app/shared/services/pixabay.service.ts
+++ src/app/shared/services/pixabay.service.ts
@@ -12,7 +12,7 @@ export class PixabayService {
   constructor(private http: HttpClient) {}
 
   searchImage(query: string, lang: string = 'de'): Observable<any> {
-    const url = `${this.apiUrl}?key=46704953-fc7d6aee67f9a46e40959cf27&q=${encodeURIComponent(query)}&lang=${lang}&image_type=photo`;
+    const url = `${this.apiUrl}?key=46704953-fc7d6aee67f9a46e40959cf27&q=${encodeURIComponent(query)}&lang=${lang}&image_type=photo&order=popular&editors_choice=true&category=food`;
     return this.http.get(url);
   }
 }
