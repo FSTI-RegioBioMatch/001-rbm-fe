776898576b05cacf62597b00b795dbfa4f38ac22
Lukas Kreuzer <98518831+LukeDerpwalka@users.noreply.github.com>
2024-11-12 08:31:49 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   THE_MEAL_DB_KEY: '9973533',
   MASTERDATA: 'https://masterdata.regiobiomatch.de/api/v1',
   //API_CORE: 'http://localhost:8082/api/v1',
-   API_CORE: 'https://core.regiobiomatch.de/api/v1',
+  API_CORE: 'https://core.regiobiomatch.de/api/v1',
   NEARBUY_API: 'https://api.staging.nearbuy-food.de/v1',
   API_PLANNER: 'https://planner.core.regiobiomatch.de/api/v1',
   //API_PLANNER: 'http://localhost:8080/api/v1',
